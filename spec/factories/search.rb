cfce49ec67627799a2e14eb8bf27d3c735e99dec
Nathan Bolser <nathan.bolser@phishme.com>
2018-03-19 08:38:57 -0600

    Add Search model and specs.
    Why:
    
    * Search objects will be first class objects. Added model and specs for
    * Search.
    
    Refs:
    
    Pair Partner:

--- spec/factories/search.rb
+++ spec/factories/search.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :search do
+<<<<<<< HEAD
     query 'rails'
+=======
+    query { ['rails', 'node', 'javascript'].sample }
+>>>>>>> Add Search model and specs.
   end
 end
